d6c5aac255ce4be944515f1ae4fb2c3a0bdb3a37
Ryan Lebel <ryan.lebel@gmail.com>
2016-01-22 14:12:42 -0500

    Update SSSnackbar.h

--- Pod/Classes/SSSnackbar.h
+++ Pod/Classes/SSSnackbar.h
@@ -64,7 +64,7 @@ IB_DESIGNABLE
 /**
  *  Presents the snackbar to the user for the configured duration of time.
  */
-- (void)show;
+- (void)show:(BOOL)showTop;
 /**
  *  Removes the snackbar from the screen. Calls the snackbar's dismissal block if one exists, unless the snackbar has its isLongRunning property set to YES and it action button has already been pressed by the user. This message is shorthand for calling dismissAnimated with YES as the argument.
  */
